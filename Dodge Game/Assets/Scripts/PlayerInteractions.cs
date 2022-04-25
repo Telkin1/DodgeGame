9c278315946c2806d6322692f925d91f34e4c292
Telkin1 <71374220+Telkin1@users.noreply.github.com>
2022-04-25 15:09:43 +0200

    Cleanup Scripts in Folder

--- Dodge Game/Assets/Scripts/PlayerInteractions.cs
+++ Dodge Game/Assets/Scripts/PlayerInteractions.cs
@@ -10,11 +10,18 @@ public class PlayerInteractions : MonoBehaviour {
   public HealthBarUI healthBarUI;
   public ObjectSpawner EnemySpawner;
 
+  private int invinciblePeriod = 0;
+
   void OnCollisionEnter2D(Collision2D collision) {
     switch (collision.gameObject.tag) {
       case "Enemy":
-        healthBarUI.AddLive(-1);
-        collision.gameObject.GetComponent<FallingObject>()?.Kill();
+        if (invinciblePeriod == 0) {
+          collision.gameObject.GetComponent<FallingObject>()?.Kill();
+          healthBarUI.AddLive(-1);
+          invinciblePeriod = 150;
+          var sr = GetComponent<SpriteRenderer>();
+          sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, sr.color.a - 0.7f);
+        }
         break;
       case "Gold":
         collision.gameObject.GetComponent<FallingObject>()?.Kill();
@@ -27,6 +34,21 @@ public class PlayerInteractions : MonoBehaviour {
     if (collision.gameObject.name.StartsWith("HealthBox")) {
       collision.gameObject.GetComponent<FallingObject>()?.Kill();
       healthBarUI.AddLive(1);
+    }
+  }
+
+  void FixedUpdate() {
+    if (invinciblePeriod <= 0) return;
+    invinciblePeriod -= 1;
+    if (invinciblePeriod == 0) {
+      var sr = GetComponent<SpriteRenderer>();
+      sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, sr.color.a + 0.7f);
+    }
+  }
+
+  void Update() {
+    if (Input.GetKey(KeyCode.S)) {
+      ScoreManager.instance.AddScore(1);
     }
   }
 }
